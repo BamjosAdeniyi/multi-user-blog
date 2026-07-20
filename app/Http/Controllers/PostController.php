05dc174360194802e0ca3cdd9401ec877823f323
Adeniyi Oluwabamise Joseph <bamisejoseph85@gmail.com>
2026-07-20 21:28:55 +0100

    Added and tested the flash message

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -41,7 +41,7 @@ class PostController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(Request $request, Post $post)
     {
         //
         $validated = $request->validate([
--- resources/views/components/flash-message.blade.php
+++ resources/views/components/flash-message.blade.php
@@ -1,7 +1,10 @@
 @if (session('success'))
+
     <div
         x-data="{ show: true }"
+        x-init="setTimeout(() => show = false, 4000)"
         x-show="show"
+        x-transition
         class="mb-6 rounded-xl border border-green-200 bg-green-50 px-5 py-4 text-green-700">
 
         <div class="flex items-center justify-between gap-4">
@@ -14,10 +17,13 @@
                 type="button"
                 @click="show = false"
                 class="text-green-700 hover:text-green-900">
+
                 &times;
+
             </button>
 
         </div>
 
     </div>
+
 @endif
