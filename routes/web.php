8cbef3aa5e353c47e0d8292c952fa567775a65ee
Adeniyi Oluwabamise Joseph <bamisejoseph85@gmail.com>
2026-07-20 19:14:22 +0100

    Added the feature to allow for guest to see and read post

--- routes/web.php
+++ routes/web.php
@@ -21,14 +21,18 @@ Route::patch('posts/{post}/publish', [PostController::class, 'publish'])
 Route::patch('/posts/{post}/unpublish', [PostController::class, 'unpublish'])
     ->middleware('auth')
     ->name('posts.unpublish');
-    
+
 // Resource Route with authentication through middleware
 Route::resource('posts', PostController::class)
-    ->middleware('auth');
+    ->middleware('auth')
+    ->except(['index', 'show']); // Making posts available to user for viewing and reading
 
-// Route::get('/dashboard', function () {
-//     return view('dashboard');
-// })->middleware(['auth', 'verified'])->name('dashboard');
+// Public routes for index and show, they both do not have authentication/middleware attached
+Route::get('/posts', [PostController::class, 'index'])
+    ->name('posts.index');
+
+Route::get('/posts/{post}', [PostController::class, 'show'])
+    ->name('posts.show');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
